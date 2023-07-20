8ddbc582b69cb4442461c5e471b70e1265573925
pragathi <saipragu@gmail.com>
2023-07-20 21:55:28 +0530

    Update data.py

--- data.py
+++ data.py
@@ -11,33 +11,7 @@ spark = SparkSession.builder\
             .config("spark.sql.execution.arrow.enabled", "true")\
             .getOrCreate()
 
-condition = None
-df_dsfdsafd = spark.read\
-    .format("jdbc")\
-    .option("url", "fdsfads")\
-    .option("user", DBUtils(spark).secrets.get(scope="dsfsdfdsfdsf", key="dsfdsf"))\
-    .option("password", DBUtils(spark).secrets.get(scope="dsfsdfdsfdsf", key="dsfsdf"))\
-    .option("dbtable", "dsfsdf")\
-    .option("driver", "fdsfdsf")\
-    .load()
-
-if condition is not None:
-    df_dsfdsafd = df_dsfdsafd.filter(condition)
+df_fdsfds = spark.read.format('json').load("dsfa")
     
-df_dsfdsafd.show()
-    
-        
-taskname = dsfdsafd.withColumn("targetname",dsfdsafd.None+234)
-
-        
-
-# Write the DataFrame to the database table
-df_taskname.write.format('jdbc')\
-    .option('url', 'databricks')\
-    .option('dbtable', 'tablename')\
-    .option("user", DBUtils(spark).secrets.get(scope="credential", key="vign@gmail.com"))\
-    .option("password", DBUtils(spark).secrets.get(scope="credential", key="sdfasd"))\
-    .option('driver', 'drivername')\
-    .mode('append')\
-    .save()
+df_fdsfds.write.format('json').save('fsdfsd', header = True)
     
